2ec6e04608fbc077acd0714b9d7489c50871e9cf
Fabian Zills <fzills@icp.uni-stuttgart.de>
2023-05-09 21:40:25 +0200

    more data

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=cpu-long
+#SBATCH --partition=gpu
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=32
-#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=64
+#SBATCH --time=20:00:00
 
 dvc repro
